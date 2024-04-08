990be64858710af75b013ee462e1ec4a7f7c5653
“Nidhi <ps4151722@gmail.com>
2024-04-08 10:06:41 +0530

    Initial Commit

---
+++ almophire_testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  almophire_testing
+//
+//  Created by Zignuts Technolab on 08/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
